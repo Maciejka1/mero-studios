7ac71f12a1b0175ab46c1063ffda1ce8eb2bf08a
Maciejka1 <maciekglab0812@vp.pl>
2022-07-23 11:48:01 +0200

    a lot css changes in nav

--- src/components/templates/nav/nav.js
+++ src/components/templates/nav/nav.js
@@ -13,18 +13,18 @@ function Nav(){
     resizeNavOnClick as function which closes nav. If user is on screen that has 769px or more it declares
     this variable as undefined because it would break styling when clicking on pc*/
     return (
-      <div>
+      <div className='mt-1'>
           <div onClick={() => setNavControl('0')}>
               <div className="open-menu fixed m-5 text-4xl top-0 right-0 md:hidden z-20" >
                   <FaBars/>
               </div>
           </div>
           <nav className="flex flex-col right-[-100%] items-center justify-center w-screen h-screen bg-white fixed md:justify-between md:w-[1224px] md:h-12 md:right-0 md:left-0 md:flex-row md:rounded-full z-20 " style={{right: navControl}}> 
-          <div className="md:h-full md:flex items-center justify-center">
-            <img src={logo} alt="logo" className="h-20 mt-8 md:m-0 md:h-full "/>
+          <div className="md:h-full md:flex-row flex-col flex items-center justify-center">
+            <img src={logo} alt="logo" className="h-20 mt-8 md:m-0 md:h-full rounded-md"/>
             <h1 className="md:ml-2 text-lg">Mero Studios</h1>
           </div>
-            <ul className="flex flex-col justify-items-center items-center mb-10 text-gray-700 gap-3 font-semibold text-4xl md:text-2xl md:mb-0 md:flex-row ">
+            <ul className="flex flex-col justify-items-center items-center mb-10 my-12 text-gray-700 gap-3 font-semibold text-4xl md:text-2xl md:mb-0 md:flex-row md:my-0">
                 <Link to="/" onClick={resizeNavOnClick}>
                     <li>Home</li>
                 </Link>
@@ -34,17 +34,17 @@ function Nav(){
             </ul>
               <ul className="flex flex-col justify-items-center items-center mb-10 text-black gap-3 font-semibold text-xl md:mb-0 md:flex-row md:text-black">
                   <a href="https://www.tiktok.com/@merostudios" target="blank">
-                      <li className='text-4xl'>
+                      <li className='text-4xl '>
                           <FaTiktok/>
                       </li>
                   </a>
                   <a href="https://discord.gg/QnxXUCqeWz" target="blank" >
-                      <li className='text-4xl'>
+                      <li className='text-4xl '>
                           <FaDiscord/>
                       </li>
                   </a>
                   <a href="https://www.youtube.com/channel/UCMcQuc_vdqUpssXgfN6Lwhg" target="blank" >
-                      <li className='text-4xl'>
+                      <li className='text-4xl '>
                           <FaYoutube/>
                       </li>
                   </a>
