b8b0bf74bfebd556a1678d22ca9305b4b77499e5
Maciejka1 <maciekglab0812@vp.pl>
2022-07-26 23:12:40 +0200

    added buttons and text

--- src/components/pages/main/main.js
+++ src/components/pages/main/main.js
@@ -7,10 +7,18 @@ export default function Main() {
     initial={{opacity: 0}}
     animate={{opacity: 1, transition: {duration: 2}}}
     exit={{opacity: 0}}
-    className="container" 
+    className="container text-xl" 
     >
-      <div className='flex justify-center items-center h-[70vh] text-6xl font-bold'>
-        <h1><span className='bg-violet-400 p-2'>Amazing</span> games done right.</h1>
+      <div className='flex justify-center items-center h-[70vh]'>
+        <div>
+          <h1 className='text-6xl font-bold leading-[80px]'><span className='bg-violet-400 p-2 '>Amazing</span> games <br/> done right.</h1>
+          {/* <h2 className='max-w-xl'>
+            Fabulous games created in Unity and Unreal Engine 5.
+            Lorem ipsum dolor sit amet?
+          </h2> */}
+          <div className='btn-primary'>See our portfolio</div>
+        </div>
+
       </div>
     </motion.div>
   )
