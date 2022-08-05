0937d6a7a3e6cb2f572e1e183aa292554b4bdd66
Maciejka1 <maciekglab0812@vp.pl>
2022-08-06 00:05:51 +0200

    contact page changes

--- src/components/pages/contact/contact.js
+++ src/components/pages/contact/contact.js
@@ -9,7 +9,9 @@ export default function Contact() {
     className="text-xl text-white">
       <section className='container mb-20'>
         <h1 className='header'>Get in touch</h1>
-        
+          <div className='border-[1px] rounded-3xl border-stone-900 p-5'>
+            <p></p>
+          </div>
       </section>
     </motion.main>
   )
