1275218fcdbb830daed0e70689af31d86ac3b199
Maciejka1 <maciekglab0812@vp.pl>
2022-08-06 14:18:23 +0200

    updated contact

--- src/components/pages/contact/contact.js
+++ src/components/pages/contact/contact.js
@@ -10,7 +10,9 @@ export default function Contact() {
       <section className='container mb-20'>
         <h1 className='header'>Get in touch</h1>
           <div className='border-[1px] rounded-3xl border-stone-900 p-5'>
-            <p></p>
+            <p className='font-bold'>Contact us on discord if you want to help us create our games!</p>
+            <p className='font-bold'>Discord: <a href="https://discord.gg/QnxXUCqeWz" target="blank" className='text-blue-700 underline font-normal'>here</a></p>
+            <p className='font-bold'>Email: <span className='font-normal'>mero@to.leniwiec</span> </p>
           </div>
       </section>
     </motion.main>
