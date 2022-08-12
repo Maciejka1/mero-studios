741ff4d17cd8045e6034e2a16b5735f6a9bfb0e4
Maciejka1 <maciekglab0812@vp.pl>
2022-08-12 23:41:40 +0200

    refactor code in about to be more readable

--- src/components/pages/main/about.js
+++ src/components/pages/main/about.js
@@ -5,20 +5,20 @@ export function About() {
     <h1 className='header'>About us</h1>
         <div>
             <p className='max-w-3xl'>
-            Mero studios is a group of game developers who make indie games on following engines:
+                Mero studios is a group of game developers who make indie games on following engines:
             </p>
             <ul className='list-disc ml-10'>
-            <li>Unreal Engine 5 (Blueprints) </li>
-            <li>Unity (C#) </li>
+                <li>Unreal Engine 5 (Blueprints)</li>
+                <li>Unity (C#)</li>
             </ul>
             <p>
-            Our team:
+                Our team:
             </p>
             <ul className='list-disc ml-10'>
-            <li>Mero - Main developer, Game designer</li>
-            <li>Dawijas - Sound engineer</li>
-            <li>Gofrywplynie - Social media</li>
-            <li>Maciejka - Web developer</li>
+                <li>Mero - Main developer, Game designer</li>
+                <li>Dawijas - Sound engineer</li>
+                <li>Gofrywplynie - Social media</li>
+                <li>Maciejka - Web developer</li>
             </ul>
         </div>
     </section>
