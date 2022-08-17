0e73b7538add1c09b67bf6ac8c0186d4d49f1e80
Maciejka1 <maciekglab0812@vp.pl>
2022-08-17 23:13:42 +0200

    fix game.js on chromium based browsers

--- src/components/templates/game/game.js
+++ src/components/templates/game/game.js
@@ -1,21 +1,24 @@
 import React from 'react'
 export default function Game(props) {
   return (
-    <div className="h-[100vh] overflow-hidden flex flex-col snap-start">
-    <div className={`h-[100%] lg:w-[50%] lg:absolute flex flex-col bg-[#0f1015] justify-center items-center ${props.styleRight && "lg:right-0"}`} >
-      <div className='grid gap-4 p-2'>
-        <h1 className='header'>{props.title}</h1>
-        <p className='max-w-xl'>{props.desc}</p>
-        <div className='flex flex-wrap gap-2'>
-          <a href={props.download} target="blank" className='btn bg-violet-400 hover:bg-violet-500 transition-all'>
-            Download
-          </a>
-        </div>
+<div className="h-[100vh] flex justify-center items-center gap-16">
+  <div className={`h-[100%] flex flex-col bg-[#0f1015] justify-center items-center ${props.styleRight && "lg:right-0"}`} >
+    <div className='grid gap-4 p-2'>
+      <h1 className='header'>{props.title}</h1>
+      <p className='max-w-xl text-2xl'>{props.desc}</p>
+      <div className='flex flex-wrap gap-2'>
+        <a href={props.download} target="blank" className='btn bg-violet-400 hover:bg-violet-500 transition-all'>
+          Download
+        </a>
       </div>
     </div>
-    <video autoPlay loop muted className="md:object-cover">
-        <source src={props.video} type="video/mp4" />
+  </div>
+
+  <div >
+    <video autoPlay loop muted>
+      <source src={props.video} type="video/mp4" className='rounded-xl' />
     </video>
+  </div>
 </div>
   )
 }
